2bb2c490128cc24d1b8813953c11ad67f69a5d08
campayoo <pcampayo07@gmail.com>
2026-03-05 14:14:46 +0100

    Implementación de mejora en el catálogo

--- js/script.js
+++ js/script.js
@@ -58,7 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
         multiplier: 1.0,
         gameList: [
             'juegos/juego1/index.html',
-            'juegos/OceanCleanup/index.html'
+            'juegos/OceanCleanup/index.html',
+            'juegos/juego3/index.html'
         ] // Add more as they deploy
     };
 
